576e74bdf1948879ec54df9bd4e974f4d60c529e
lha5268 <lhavemane5268@conestogac.on.ca>
2024-11-27 17:01:14 -0500

    Rough homepage, rough profile, rough map

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,23 +2,57 @@ package com.example.myapplication;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Button;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button buttonProfile;
+    Button buttonMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        buttonProfile = (Button) findViewById(R.id.button_id_profile);
+        buttonProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openProfile();
+            }
+        });
+
+        buttonMap = (Button) findViewById(R.id.button_id_map);
+        buttonMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMap();
+            }
         });
     }
+
+    public void openProfile(){
+        Intent intent = new Intent(this, Profile.class);
+        startActivity(intent);
+    }
+    public void openMap(){
+        Intent intent = new Intent(this, Map.class);
+        startActivity(intent);
+    }
+    public void startTrip(View view){
+        // Logic to get data here
+        final TextView textTitle=findViewById(R.id.text_title);
+        textTitle.setText("The trip has started!");
+        final TextView textScore=findViewById(R.id.text_score);
+        textScore.setText("Loading...");
+    }
+    public void endTrip(View view){
+        // logic to calculate and display score here
+        final TextView textTitle=findViewById(R.id.text_title);
+        textTitle.setText("Insurance Application");
+        final TextView textScore=findViewById(R.id.text_score);
+        textScore.setText("50%");
+    }
+
 }
---
+++ MyApplication/app/src/main/java/com/example/myapplication/Profile.java
@@ -0,0 +1,17 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+public class Profile extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+    public void toHome(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
