8c0b47b8469ece645b4b336abe0f3da4f3ff2554
lha5268 <lhavemane5268@conestogac.on.ca>
2024-11-28 17:58:12 -0500

    Improved GUI, with reorganization

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,55 +4,41 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
-import android.widget.Button;
 import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
-    Button buttonProfile;
-    Button buttonMap;
+    ImageButton btnProfile;
+    ImageButton btnMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        buttonProfile = (Button) findViewById(R.id.button_id_profile);
-        buttonProfile.setOnClickListener(new View.OnClickListener() {
+        btnProfile = (ImageButton) findViewById(R.id.btn_profile);
+        btnProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openProfile();
+                openProfile(v);
             }
         });
 
-        buttonMap = (Button) findViewById(R.id.button_id_map);
-        buttonMap.setOnClickListener(new View.OnClickListener() {
+        btnMap = (ImageButton) findViewById(R.id.btn_drive);
+        btnMap.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openMap();
+                openMap(v);
             }
         });
     }
 
-    public void openProfile(){
+    public void openProfile(View view){
         Intent intent = new Intent(this, Profile.class);
         startActivity(intent);
     }
-    public void openMap(){
+    public void openMap(View view){
         Intent intent = new Intent(this, Map.class);
         startActivity(intent);
-    }
-    public void startTrip(View view){
-        // Logic to get data here
-        final TextView textTitle=findViewById(R.id.text_title);
-        textTitle.setText("The trip has started!");
-        final TextView textScore=findViewById(R.id.text_score);
-        textScore.setText("Loading...");
-    }
-    public void endTrip(View view){
-        // logic to calculate and display score here
-        final TextView textTitle=findViewById(R.id.text_title);
-        textTitle.setText("Insurance Application");
-        final TextView textScore=findViewById(R.id.text_score);
-        textScore.setText("50%");
     }
 
 }
--- MyApplication/app/src/main/java/com/example/myapplication/Map.java
+++ MyApplication/app/src/main/java/com/example/myapplication/Map.java
@@ -2,17 +2,50 @@ package com.example.myapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.content.Intent;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 public class Map extends AppCompatActivity {
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
     }
 
-    public void toHome(){
+    public void toHome(View view){
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+    }
+
+    public void openProfile(View view){
+        Intent intent = new Intent(this, Profile.class);
+        startActivity(intent);
+    }
+
+//    public void openMap(View view){
+//        Intent intent = new Intent(this, Map.class);
+//        startActivity(intent);
+//    }
+
+    public void startTrip(View view){
+        // Logic to get data here
+        final TextView textTitle=findViewById(R.id.text_title);
+        textTitle.setText("The trip has started!");
+        final TextView textScore=findViewById(R.id.text_score);
+        textScore.setText("Loading...");
+    }
+    public void endTrip(View view){
+        // logic to calculate and display score here
+        final TextView textTitle=findViewById(R.id.text_title);
+        textTitle.setText("Insurance Application");
+        final TextView textScore=findViewById(R.id.text_score);
+        textScore.setText("50%");
     }
 }
