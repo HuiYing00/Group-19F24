5a1cb794d156ce1e533f6b5d07002d4b5fce96a0
lha5268 <lhavemane5268@conestogac.on.ca>
2024-11-29 00:24:46 -0500

    Trying to get googleMaps to work, so far unsuccessful

--- MyApplication/app/src/main/java/com/example/myapplication/Map.java
+++ MyApplication/app/src/main/java/com/example/myapplication/Map.java
@@ -1,22 +1,99 @@
 package com.example.myapplication;
 
-import android.content.Intent;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.content.Intent;
+import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
-public class Map extends AppCompatActivity {
+import java.util.Objects;
 
+public class Map extends AppCompatActivity implements OnMapReadyCallback {
+
+    private final int PERM_FINE_LOCATION = 1;
+
+    Location currentLocation;
+    FusedLocationProviderClient fusedLocationProviderClient;
+
+    private GoogleMap myMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        getLastLocation();
+    }
+
+    private void getLastLocation()
+    {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERM_FINE_LOCATION);
+            return;
+        }
+        Task<Location> task = fusedLocationProviderClient.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null)
+                {
+                    currentLocation = location;
+
+                    SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+                    mapFragment.getMapAsync(Map.this);
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap)
+    {
+        myMap = googleMap;
+        LatLng kitch = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+        myMap.addMarker(new MarkerOptions().position(kitch).title("Kitchener"));
+        myMap.moveCamera(CameraUpdateFactory.newLatLng(kitch));
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERM_FINE_LOCATION)
+        {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            {
+                getLastLocation();
+
+            }
+            else
+            {
+                Toast.makeText(this, "Location permission is denied. Please allow location to use this feature.", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
     public void toHome(View view){
@@ -36,16 +113,16 @@ public class Map extends AppCompatActivity {
 
     public void startTrip(View view){
         // Logic to get data here
-        final TextView textTitle=findViewById(R.id.text_title);
-        textTitle.setText("The trip has started!");
-        final TextView textScore=findViewById(R.id.text_score);
-        textScore.setText("Loading...");
+//        final TextView textTitle=findViewById(R.id.text_title);
+//        textTitle.setText("The trip has started!");
+//        final TextView textScore=findViewById(R.id.text_score);
+//        textScore.setText("Loading...");
     }
     public void endTrip(View view){
         // logic to calculate and display score here
-        final TextView textTitle=findViewById(R.id.text_title);
-        textTitle.setText("Insurance Application");
-        final TextView textScore=findViewById(R.id.text_score);
-        textScore.setText("50%");
+//        final TextView textTitle=findViewById(R.id.text_title);
+//        textTitle.setText("Insurance Application");
+//        final TextView textScore=findViewById(R.id.text_score);
+//        textScore.setText("50%");
     }
 }
